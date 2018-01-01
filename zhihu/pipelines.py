2bfb2c9e0429c802bb4fefdcc24133a88d493237
Dreag <yang_g1@126.com>
2018-01-01 18:45:49 +0800

    增加mysql数据库存储

--- zhihu/pipelines.py
+++ zhihu/pipelines.py
@@ -6,7 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import json
-# import pymysql
+import pymysql
 import codecs
 
 class ZhihuPipeline(object):
@@ -16,7 +16,7 @@ class ZhihuPipeline(object):
 class ShipPipeline(object):
     # 自定义导出json
     def __init__(self):
-        self.file = codecs.open('data2.json', 'w', encoding='utf-8')
+        self.file = codecs.open('relation.json', 'w', encoding='utf-8')
     
     # 处理结束后关闭文件IO流
     def close_spider(self, spider):
@@ -26,4 +26,26 @@ class ShipPipeline(object):
     def process_item(self, item, spider):
         lines = json.dumps(dict(item), ensure_ascii=False) + '\n'
         self.file.write(lines)
+        return item
+
+
+
+class DatabasePipeline(object):
+    """docstring for DatabasePipeline"""
+    def __init__(self):
+        self.conn = pymysql.connect(host = 'localhost',port = 3306,user = 'root',passwd = 'passwd',db = 'zhihu',charset = 'utf8')
+        self.cursor = self.conn.cursor()
+        # truncate为数据库删除操作
+        # self.cursor.execute("truncate table people_inform")
+        print("连接成功")
+        self.conn.commit()
+
+
+    def process_item(self, item, spider):
+        sql = 'insert into people_inform(id,name,url_token,user_type,answer_count,articles_count,follower_count) values (%s,%s,%s,%s,%s,%s,%s)'
+        try:
+            self.cursor.execute(sql,(item['id'],item['name'],item['url_token'],item['user_type'],item['answer_count'],item['articles_count'],item['follower_count']))
+            self.conn.commit()
+        except pymysql.Error:
+            print("error,DatabasePipeline.")
         return item
